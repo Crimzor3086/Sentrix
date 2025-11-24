9e1020d880f3dd56aeded4579a244a97ec45f3b1
Crimzor <crimzor@kali.Innovah>
2025-11-24 03:07:00 -0700

    update

---
+++ hardhat.config.cjs
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-ethers");
+require("dotenv").config();
+
+const { MANTLE_SEPOLIA_RPC_URL, PRIVATE_KEY } = process.env;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+  paths: {
+    sources: "./contracts",
+  },
+  networks: {
+    mantleSepolia: {
+      url: MANTLE_SEPOLIA_RPC_URL || "",
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
+  },
+};
+
